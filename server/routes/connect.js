c1491d4269689f812ab61f585ca3906f9a3d9283
SometimesSomething <sometimequixoticor@gmail.com>
2019-02-18 09:40:42 +0800

    删除了重复server

--- server/routes/connect.js
+++
@@ -1,1 +0,0 @@
-//存入数据库const mysql = require("mysql");const connection = mysql.createConnection({    host:"localhost",    user:"root",    password:"root",    port:"33060",    database:"smms"});connection.connect(()=>{    console.log("连接数据库成功")});module.exports = connection;
